39c26bca80f70a33edb0f98d3e11dde18fc24e4d
puddy <puddy@reggiD>
2016-01-23 15:03:27 +0100

    -piwik added

--- template/overall-footer.tpl.php
+++ template/overall-footer.tpl.php
@@ -33,7 +33,9 @@
 
 			var newsticker1 = $('#newsticker1 .newsitem').newsTicker({
 				row_height: 100,
-				speed: 800
+				speed: 800,
+				max_row: 1,
+				duration: 5000
 			});
 		
 			var roxyFileman = 'libs/fileman/index.html'; 
@@ -47,7 +49,21 @@
 
 		</script>
 	
-	
+		<!-- Piwik -->
+		<script type="text/javascript">
+			var _paq = _paq || [];
+			_paq.push(['trackPageView']);
+			_paq.push(['enableLinkTracking']);
+			(function() {
+				var u="//www.reggid213.tk/piwik/";
+				_paq.push(['setTrackerUrl', u+'piwik.php']);
+				_paq.push(['setSiteId', 1]);
+				var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+				g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+			})();
+		</script>
+		<noscript><p><img src="//www.reggid213.tk/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+		<!-- End Piwik Code -->
 	</body>
 
 </html>
