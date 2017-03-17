dbe0426111216c53686de9e43a1c39a1a2812089
reggid213 <reggiD213@gmail.com>
2017-03-17 10:27:35 +0100

    -removed cubus, removed links from sidebar

--- template/sidebar_regular.tpl.php
+++ template/sidebar_regular.tpl.php
@@ -3,15 +3,14 @@
 	<hr>
 	<ul class="box regular layout">
 		<!--summer <div class="summerbreak">-->
-			<li><a href="#"><h3 class="glow">Jeden 1. Samstag im Monat</h3><img src="<?php echo BASEPATH; ?>/images/good-old-shit.jpg" alt="Good Old Shit"></a></li>
-			<li><a href="#"><h3 class="glow">Jeden 2. Samstag im Monat</h3><img src="<?php echo BASEPATH; ?>/images/valley-of-doom.jpg" alt="Valley of Doom"></a></li>
-			<li><a href="#"><h3 class="glow">Jeden 3. Samstag im Monat</h3><img src="<?php echo BASEPATH; ?>/images/full-metal-night.jpg" alt="Full Metal Night"></a></li>
+			<li><h3 class="glow">Jeden 1. Samstag im Monat</h3><img src="<?php echo BASEPATH; ?>/images/good-old-shit.jpg" alt="Good Old Shit"></li>
+			<li><h3 class="glow">Jeden 2. Samstag im Monat</h3><img src="<?php echo BASEPATH; ?>/images/valley-of-doom.jpg" alt="Valley of Doom"></li>
+			<li><h3 class="glow">Jeden 3. Samstag im Monat</h3><img src="<?php echo BASEPATH; ?>/images/full-metal-night.jpg" alt="Full Metal Night"></li>
+            <!--<li><a target="_blank" href="http://www.cubus-ulm.de"><h3 class="glow">Jeden Montag und Mittwoch</h3><img src="<?php echo BASEPATH; ?>/images/cubus.png" alt="Cubus"></a></li>-->
+            <li><h3 class="glow">Jeden Donnerstag</h3><img src="<?php echo BASEPATH; ?>/images/tabletop.jpg" alt="Tabletop"></li>
+        <!--summer	<img class="summerpic" src="<?php echo BASEPATH; ?>/images/summerbreak.png"">
+        </div>-->
 
-		<!--summer	<img class="summerpic" src="<?php echo BASEPATH; ?>/images/summerbreak.png"">
-		</div>-->
 
-
-		<li><a target="_blank" href="http://www.cubus-ulm.de"><h3 class="glow">Jeden Montag und Mittwoch</h3><img src="<?php echo BASEPATH; ?>/images/cubus.png" alt="Cubus"></a></li>
-		<li><a target="_blank" href="http://www.cubus-ulm.de"><h3 class="glow">Jeden Donnerstag</h3><img src="<?php echo BASEPATH; ?>/images/tabletop.jpg" alt="Tabletop"></a></li>
 	</ul>
 </div>
